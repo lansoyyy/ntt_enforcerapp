ff69b15527e16657c67e14661469885e0ac863c2
Lans12345 <olanalans12345@gmail.com>
2024-07-12 17:45:26 +0800

    changed textfields widget

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:enforcer_app/screens/home_screen.dart';
 import 'package:enforcer_app/widgets/button_widget.dart';
 import 'package:enforcer_app/widgets/textfield_widget.dart';
 import 'package:flutter/material.dart';
@@ -12,52 +13,82 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final username = TextEditingController();
   final password = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Logo Placeholder
-              const CircleAvatar(
-                radius: 100,
-                backgroundColor: Colors.transparent,
-                backgroundImage: AssetImage(
-                  'assets/images/logo.png',
-                ), // Replace with your logo asset
-              ),
-
-              const Text(
-                'Welcome to Traffic Violation Ticketing System',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Logo Placeholder
+                const CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.transparent,
+                  backgroundImage: AssetImage(
+                    'assets/images/logo.png',
+                  ), // Replace with your logo asset
                 ),
-              ),
-              const SizedBox(height: 10),
-              TextFieldWidget(
-                label: 'Username',
-                isRequred: true,
-                controller: username,
-              ),
-              const SizedBox(height: 10),
-              TextFieldWidget(
-                label: 'Password',
-                isRequred: true,
-                isObscure: true,
-                controller: password,
-              ),
-              const SizedBox(height: 30),
-              ButtonWidget(
-                label: 'Login',
-                onPressed: () {},
-              ),
-            ],
+
+                const Text(
+                  'Welcome to Traffic Violation Ticketing System',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextFieldWidget(
+                  label: 'Username',
+                  controller: username,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a username';
+                    }
+
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10),
+                TextFieldWidget(
+                  label: 'Password',
+                  isObscure: true,
+                  controller: password,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    if (value.length < 8) {
+                      return 'Password must be at least 8 characters long';
+                    }
+
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 30),
+                ButtonWidget(
+                  label: 'Login',
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(
+                            builder: (context) => const HomeScreen()),
+                        (route) {
+                          return false;
+                        },
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/textfield_widget.dart
+++ lib/widgets/textfield_widget.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:enforcer_app/utlis/colors.dart';
+import 'package:enforcer_app/widgets/text_widget.dart';
 
 class TextFieldWidget extends StatefulWidget {
+  final String label;
   final String? hint;
-  final String? label;
   bool? isObscure;
   final TextEditingController controller;
   final double? width;
-  final IconData? prefixIcon;
   final double? height;
   final int? maxLine;
   final TextInputType? inputType;
@@ -17,37 +16,27 @@ class TextFieldWidget extends StatefulWidget {
   late Color? hintColor;
   late double? radius;
   final String? Function(String?)? validator; // Add validator parameter
-  bool? isEnabled;
-  final TextCapitalization? textCapitalization;
-  final FontStyle? fontStyle;
-  final String? errorText;
-  final bool? isRequred;
-  bool? showErrorMsg;
 
-  TextFieldWidget(
-      {super.key,
-      this.hint = '',
-      this.isRequred = true,
-      this.fontStyle = FontStyle.italic,
-      this.label,
-      this.prefixIcon,
-      required this.controller,
-      this.isObscure = false,
-      this.width = 275,
-      this.height = 50,
-      this.maxLine = 1,
-      this.hintColor = Colors.white,
-      this.borderColor = const Color(0xff1A1851),
-      this.showEye = false,
-      this.color = Colors.white,
-      this.radius = 5,
-      this.textCapitalization = TextCapitalization.sentences,
-      this.inputType = TextInputType.text,
-      this.validator,
-      this.errorText,
-      this.showErrorMsg = true,
-      this.isEnabled = true // Add validator parameter
-      });
+  final TextCapitalization? textCapitalization;
+
+  TextFieldWidget({
+    super.key,
+    required this.label,
+    this.hint = '',
+    required this.controller,
+    this.isObscure = false,
+    this.width = double.infinity,
+    this.height = 40,
+    this.maxLine = 1,
+    this.hintColor = Colors.white,
+    this.borderColor = Colors.transparent,
+    this.showEye = false,
+    this.color = Colors.white,
+    this.radius = 5,
+    this.textCapitalization = TextCapitalization.sentences,
+    this.inputType = TextInputType.text,
+    this.validator, // Add validator parameter
+  });
 
   @override
   State<TextFieldWidget> createState() => _TextFieldWidgetState();
@@ -56,134 +45,76 @@ class TextFieldWidget extends StatefulWidget {
 class _TextFieldWidgetState extends State<TextFieldWidget> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 5, bottom: 5),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: widget.label,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontFamily: 'Bold',
-                    color: primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                widget.isRequred!
-                    ? const TextSpan(
-                        text: '*',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontFamily: 'Bold',
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    : const TextSpan(
-                        text: '',
-                        style: TextStyle(
-                          fontSize: 0,
-                          fontFamily: 'Bold',
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-              ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextWidget(text: widget.label, fontSize: 12, color: widget.hintColor!),
+        const SizedBox(
+          height: 5,
+        ),
+        TextFormField(
+          style: const TextStyle(
+            fontFamily: 'QRegular',
+            fontSize: 14,
+          ),
+          textCapitalization: widget.textCapitalization!,
+          keyboardType: widget.inputType,
+          decoration: InputDecoration(
+            suffixIcon: widget.showEye! == true
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        widget.isObscure = !widget.isObscure!;
+                      });
+                    },
+                    icon: widget.isObscure!
+                        ? const Icon(Icons.visibility)
+                        : const Icon(Icons.visibility_off))
+                : const SizedBox(),
+            filled: true,
+            fillColor: Colors.white,
+            hintText: widget.hint,
+            border: InputBorder.none,
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: widget.borderColor!,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: widget.borderColor!,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: widget.borderColor!,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Colors.red,
+              ),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            errorStyle: const TextStyle(fontFamily: 'QBold', fontSize: 12),
+            focusedErrorBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Colors.red,
+              ),
+              borderRadius: BorderRadius.circular(5),
             ),
           ),
-          const SizedBox(
-            height: 5,
-          ),
-          SizedBox(
-            width: widget.width,
-            height: widget.height,
-            child: TextFormField(
-              enabled: widget.isEnabled,
-              style: TextStyle(
-                fontFamily: 'Regular',
-                fontSize: 14,
-                color: primary,
-              ),
-              textCapitalization: widget.textCapitalization!,
-              keyboardType: widget.inputType,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: grey.withOpacity(0.2),
-                suffixIcon: widget.showEye! == true
-                    ? IconButton(
-                        onPressed: () {
-                          setState(() {
-                            widget.isObscure = !widget.isObscure!;
-                          });
-                        },
-                        icon: widget.isObscure!
-                            ? Icon(
-                                Icons.visibility,
-                                color: primary,
-                              )
-                            : Icon(
-                                Icons.visibility_off,
-                                color: primary,
-                              ))
-                    : const SizedBox(),
-                contentPadding: const EdgeInsets.fromLTRB(20.0, 0, 20.0, 0),
-                hintStyle: TextStyle(
-                  fontStyle: widget.fontStyle!,
-                  fontFamily: 'Regular',
-                  fontSize: 14,
-                  color: primary,
-                ),
-                hintText: widget.hint,
-                border: InputBorder.none,
-                disabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: widget.borderColor!,
-                  ),
-                  borderRadius: BorderRadius.circular(widget.radius!),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: widget.borderColor!,
-                  ),
-                  borderRadius: BorderRadius.circular(widget.radius!),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: widget.borderColor!,
-                  ),
-                  borderRadius: BorderRadius.circular(widget.radius!),
-                ),
-                errorBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.red,
-                  ),
-                  borderRadius: BorderRadius.circular(widget.radius!),
-                ),
-                errorText: widget.errorText,
-                errorStyle: TextStyle(
-                    fontFamily: 'Bold',
-                    fontSize: widget.showErrorMsg! ? widget.radius! : 0.1),
-                focusedErrorBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.red,
-                  ),
-                  borderRadius: BorderRadius.circular(widget.radius!),
-                ),
-              ),
 
-              maxLines: widget.maxLine,
-              obscureText: widget.isObscure!,
-              controller: widget.controller,
-              validator:
-                  widget.validator, // Pass the validator to the TextFormField
-            ),
-          ),
-        ],
-      ),
+          maxLines: widget.maxLine,
+          obscureText: widget.isObscure!,
+          controller: widget.controller,
+          validator:
+              widget.validator, // Pass the validator to the TextFormField
+        ),
+      ],
     );
   }
 }
