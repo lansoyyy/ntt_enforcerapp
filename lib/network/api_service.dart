fa9e2f7a755c32b3c4c17e450bf2977fb3901c23
Lans12345 <olanalans12345@gmail.com>
2024-08-08 12:56:22 +0800

    added api service

---
+++ lib/network/api_service.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'package:enforcer_app/network/endpoints.dart';
+import 'package:http/http.dart' as http;
+
+class Network {
+
+
+
+  Future<http.Response> get(String endpoint) async {
+    final url = Uri.parse('${ApiEndpoints.baseUrl}$endpoint');
+    final response = await http.get(url);
+    _checkStatusCode(response);
+    return response;
+  }
+
+  Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
+    final url = Uri.parse('${ApiEndpoints.baseUrl}$endpoint');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(body),
+    );
+    _checkStatusCode(response);
+    return response;
+  }
+
+  Future<http.Response> put(String endpoint, Map<String, dynamic> body) async {
+    final url = Uri.parse('${ApiEndpoints.baseUrl}$endpoint');
+    final response = await http.put(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(body),
+    );
+    _checkStatusCode(response);
+    return response;
+  }
+
+  Future<http.Response> delete(String endpoint) async {
+    final url = Uri.parse('${ApiEndpoints.baseUrl}$endpoint');
+    final response = await http.delete(url);
+    _checkStatusCode(response);
+    return response;
+  }
+
+  void _checkStatusCode(http.Response response) {
+    if (response.statusCode < 200 || response.statusCode >= 300) {
+      throw Exception('Failed request: ${response.statusCode}');
+    }
+  }
+}
---
+++ lib/network/endpoints.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: file_names
+
+class ApiEndpoints {
+  static const String baseUrl = "http://192.168.0.150:8000";
+
+  
+
+   
+
+}
