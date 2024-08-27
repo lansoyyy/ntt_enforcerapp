3378011380afb6b60b6897615bb759e22e93690a
Lans12345 <olanalans12345@gmail.com>
2024-08-27 14:11:29 +0800

    fixed endpoints

--- lib/network/endpoints.dart
+++ lib/network/endpoints.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: file_names
 
 class ApiEndpoints {
-  static const String baseUrl = "https://api.ticketlink.ph/";
+  static const String baseUrl = "https://api.ticketlink.ph/api/v1/";
 }
